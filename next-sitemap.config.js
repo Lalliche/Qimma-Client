3fffd42157b09149e1822138f9b13c538402f388
Lalliche <abdelhadi.lalliche@gmail.com>
2025-06-30 15:51:33 +0100

    adding sitemapping

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://qimmalearning.com", // ✅ your live domain here
+  generateRobotsTxt: true, // ✅ also create robots.txt
+  sitemapSize: 5000,
+  changefreq: "weekly",
+  priority: 0.7,
+};
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,10 +3,44 @@ import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 import "@/app/globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import type { Metadata } from "next";
 
 interface Props {
   children: React.ReactNode;
   params: Promise<{ locale: string }>;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Qimma",
+    description: "You're closer to the top than you think.",
+    robots: "index, follow",
+    openGraph: {
+      title: "Qimma",
+      description: "You're closer to the top than you think.",
+      url: "https://qimmalearning.com",
+      siteName: "Qimma",
+      images: [
+        {
+          url: "/thumbnail.png",
+          width: 1200,
+          height: 630,
+          alt: "Qimma preview image",
+        },
+      ],
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Qimma",
+      description: "You're closer to the top than you think.",
+      images: ["/thumbnail.png"],
+    },
+
+    icons: {
+      icon: "/icon.ico",
+    },
+  };
 }
 
 export default async function LocaleLayout({ children, params }: Props) {
@@ -25,14 +59,13 @@ export default async function LocaleLayout({ children, params }: Props) {
       className={`scroll-smooth ${locale === "ar" ? "locale-ar" : "locale-en"}`}
     >
       <body
-        className={`bg-[#eef2ff] text-[12px]  md:text-[14px] lg:text-[15px] xl:text-[16px] xxl:text-[20px] `}
+        className={`bg-[#eef2ff] text-[12px] md:text-[14px] lg:text-[15px] xl:text-[16px] xxl:text-[20px]`}
         style={{
           fontFamily: isRTL ? "RH-ZAK, sans-serif" : "",
         }}
       >
         <NextIntlClientProvider locale={locale} messages={messages}>
           {children}
-
           <Analytics />
         </NextIntlClientProvider>
       </body>
